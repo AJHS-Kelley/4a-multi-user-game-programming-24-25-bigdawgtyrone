104e44f313b1bfdf49090f5dfc2e1d857528722a
bigdawgtyrone <burnedtoast48@gmail.com>
2024-09-18 14:55:53 -0400

    beginning of rps code

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rps.cs
@@ -0,0 +1,37 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    
+string playerName = "";
+int playerScore = 0;
+int cpuScore = 0;
+string playerChoice = "";
+string cpuChoice = "";
+string gameChoice = "";
+
+  Console.WriteLine("CPU: What is your name...? (Type a name then press Enter).\n");
+  playerName = Console.ReadLine();
+  Console.WriteLine("CPU: so your " + playerName + "huh...");
+  Console.WriteLine("CPU: would you be intersted in a legendary battle of Rock ,Paper , Scissors?");
+  gameChoice = Console.ReadLine();
+
+  if (gameChoice = yes || Yes || YES)
+  {
+    Console.WriteLine("CPU: Ok lets start... Choose rock, paper, or scissors.");
+  }
+  else if (gameChoice = maybe || Maybe || MAYBE)
+  {
+    Console.WriteLine("CPU: Make up your mind then return...");
+  }
+  else
+  {
+    Console.WriteLine("CPU: Thats what i thought...");
+    break
+  }
+  
+
+
+  
+
+  }
+}
