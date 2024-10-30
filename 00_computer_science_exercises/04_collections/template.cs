3351214418e333924514f0d887c232818412008b
bigdawgtyrone <burnedtoast48@gmail.com>
2024-10-30 12:50:01 -0400

    fixed arrays

--- 00_computer_science_exercises/04_collections/template.cs
+++ 00_computer_science_exercises/04_collections/template.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 
 
-class HelloWorld {
+class Collections {
   static void Main() {
  
  /*
@@ -22,14 +22,20 @@ class HelloWorld {
   // DECLARE AND DEFINING AN ARRAY
   string[] breakfastFoods = {"cheeseburger", "double cheeseburger", "triple cheeseburger", "Chicken bigmac", "Waffles"};
   int[] testScores = {70, 74, 80, 84, 90};
-  double[] GPA = {1.8D, 2.4D, 6.3D, 3.8D};
+  double[] GPA = {1.8D, 2.4D, 6.3D, 3.8D, 0.000001};
 
   // Print Array Contents -- All Elements on Single Line
-  Console.WriteLine("The elements of each barray are:\n");
+  Console.WriteLine("The elements of each array are:\n");
   Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
   Console.WriteLine(); // Print an empty line to the screen.
 
+  Console.WriteLine("The elements of each array are:\n");
+  Console.WriteLine("testScores: \n" + String.Join(", ", testScores));
+  Console.WriteLine(); 
 
+  Console.WriteLine("The elements of each array are:\n");
+  Console.WriteLine("GPA's: \n" + String.Join(", ", GPA));
+  Console.WriteLine(); 
 
 
   } // DON'T DELETEEEEE (3 spaces)!!!!
